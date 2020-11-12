f1303b1cac8fb8c5da139ee637b87f1aa8f7b6bd
Arsenic <vector2912@gmail.com>
2020-11-12 11:06:35 +0530

    Updated gitignore

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const axios = require('axios');
+const cheerio = require('cheerio')
 
 
-axios.get("https://api.thedogapi.com/v1/images/search")
-	.then(result => {
-		url = result.data[0].url;
-		console.log(url)
-	})
-	.catch(err => console.log(err))
+// axios.get("https://api.thedogapi.com/v1/images/search")
+// 	.then(result => {
+// 		url = result.data[0].url;
+// 		console.log(url)
+// 	})
+// 	.catch(err => console.log(err))
+
+url = "https://www.worldometers.info/coronavirus/country/india/"
+
+axios.get(url)
+        .then(response => {
+            const $ = cheerio.load(response.body)
+            console.log(r('maincounter-number'))
+        })
