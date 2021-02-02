c4c2b3be02e331adc6155404fad93a44398aeddf
Arsenic <vector2912@gmail.com>
2021-02-02 12:14:37 +0530

    bug fixed in covid.js

--- services/covid.js
+++ services/covid.js
@@ -9,7 +9,6 @@ const fetchHTML = async (url) => {
 const getCovidData = async (country) => {
 	baseURL = `https://www.worldometers.info/coronavirus/country/${country}`;
 	const data = fetchHTML(baseURL);
-
 	return data.then((result) => {
 			const countryName = result('.label-counter').text().split('/')[2].trim();
 			const flagURL =
@@ -43,13 +42,12 @@ const getCovidData = async (country) => {
 
 const covid = async (country) => {
 	const result = await getCovidData(country);
-	
 	if (result.status) {
 		return {
 			markdown: `🦠 *Covid:\t ${result.data.countryName}*\n\n` +
 				`*Total Cases:*\t ${result.data.data[0]}\n` +
 				`*Total Deaths:*\t ${result.data.data[1]}\n` +
-				`*Total Recovered:*\t ${result.data.data[0]}\n\n` +
+				`*Total Recovered:*\t ${result.data.data[2]}\n\n` +
 				`*Updates:*\n${result.data.update}\n\n` +
 				`[Flag](${result.data.flag})`
 		}
