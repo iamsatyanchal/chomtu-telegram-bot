0e4112e7a8fbbbcf0917aa43ab57cbd40b7fb169
Arsenic <vector2912@gmail.com>
2021-02-23 12:37:43 +0530

    Added late updated time on /covid command

--- services/covid.js
+++ services/covid.js
@@ -8,36 +8,44 @@ const fetchHTML = async (url) => {
 
 const getCovidData = async (country) => {
 	baseURL = `https://www.worldometers.info/coronavirus/country/${country}`;
+	// Call fetchHTML function and get the HTML of the page.
 	const data = fetchHTML(baseURL);
+
 	return data.then((result) => {
-			const countryName = result('.label-counter').text().split('/')[2].trim();
-			const flagURL =
-				"https://www.worldometers.info" +
-				result("div > img").attr("src");
-			// Data [Array]: Cases / Deaths / Recovered
-			const data = result("#maincounter-wrap > .maincounter-number")
-				.text()
-				.trim()
-				.split("\n\n");
-			// Updates: A sentence
-			const update = result(".news_li").text().split("[source]")[0].trim();
-			// Return the Data in JSON form
-			return {
-				status: true,
-				data: {
-					countryName,
-					flag: flagURL,
-					data,
-					update,
-				},
-			};
-		})
-		.catch((err) => {
-			return {
-				satus: false,
-				message: 'Country Not Found!'
-			};
-		});
+		// Grab the country name from returned HTML
+		const countryName = result('.label-counter').text().split('/')[2].trim();
+		// Get the FlagURL
+		const flagURL = "https://www.worldometers.info" + result("div > img").attr("src");
+		// Data [Array]: Cases / Deaths / Recovered
+		const data = result("#maincounter-wrap > .maincounter-number")
+			.text()
+			.trim()
+			.split("\n\n");
+		// Updates: A sentence
+		const update = result(".news_li").text().split("[source]")[0].trim();
+
+		// Trying to dates
+		const dateDiv = result(".content-inner").text().trim().split("\n")[2];
+		const date =  new Date(dateDiv).toLocaleString(undefined, { timeZone: 'Asia/Kolkata' }) + ' (IST)';
+
+		// Return the Data in JSON form
+		return {
+			status: true,
+			data: {
+				countryName,
+				flag: flagURL,
+				data,
+				update,
+				lastUpdated: date
+			},
+		};
+	})
+	.catch((err) => {
+		return {
+			satus: false,
+			message: 'Country Not Found!'
+		};
+	});
 };
 
 const covid = async (country) => {
@@ -48,7 +56,8 @@ const covid = async (country) => {
 				`*Total Cases:*\t ${result.data.data[0]}\n` +
 				`*Total Deaths:*\t ${result.data.data[1]}\n` +
 				`*Total Recovered:*\t ${result.data.data[2]}\n\n` +
-				`*Updates:*\n${result.data.update}\n\n` +
+				`*Updates:*\n${result.data.update}\n\n` + 
+				`*Last updated:*\n${result.data.lastUpdated}\n\n` +
 				`[Flag](${result.data.flag})`
 		}
 	};
