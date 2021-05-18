d442dec064f2503bd9ca1d373e89e6e4511ab829
Arsenic <vector2912@gmail.com>
2021-05-18 12:25:16 +0530

    Added config for axios

--- src/services/fetchHTML.js
+++ src/services/fetchHTML.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import { axiosInstance } from '../../config/axiosConfig';
 import cheerio from 'cheerio';
 
 //  Fetch the HTML of a given URL
 export default async function fetchHTML(url)  {
-	const { data } = await axios.get(url);
+	const { data } = await axiosInstance.get(url);
 	return cheerio.load(data);
 };
