4ead44ff910afcee1fcbbf4732728a600b107224
Astrix <vector2912@gmail.com>
2021-07-20 08:28:45 +0000

    ddg

--- src/commands/utility/ddg.js
+++ src/commands/utility/ddg.js
@@ -8,7 +8,6 @@ module.exports =  {
   chatAction: 'typing',
   async execute(ctx, query) {
     const result = await ddg(query);
-    console.log(result.markdown)
     ctx.replyWithMarkdown(result.markdown);
   }
 }
