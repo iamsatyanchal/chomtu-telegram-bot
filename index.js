2ff5a6ab0dbafae09ad3c6461d6fd7d45d968a8f
Arsenic <vector2912@gmail.com>
2021-08-22 11:59:40 +0530

    fixed spacing

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { BRAD_API, BOT_API } from './src/config';
-const { Telegraf } = require("telegraf");
+const { Telegraf } = require('telegraf');
 const cron = require('node-cron');
 
 const bot = new Telegraf(BOT_API);
@@ -14,54 +14,58 @@ const collection = new Map();
 
 // Get all the commands and put them in collection map
 for (const folder of commandFolders) {
-	// Get files from each command folder
-	const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const command = require(`./src/commands/${folder}/${file}`);
-		collection.set(command.name, command);
-	}
+  // Get files from each command folder
+  const commandFiles = fs
+    .readdirSync(`./src/commands/${folder}`)
+    .filter((file) => file.endsWith('.js'));
+  for (const file of commandFiles) {
+    const command = require(`./src/commands/${folder}/${file}`);
+    collection.set(command.name, command);
+  }
 }
 
-bot.on("message", (ctx) => {
+bot.on('message', (ctx) => {
+  if (!ctx.message.text || !ctx.message.text.startsWith('/')) return;
 
-	if (!ctx.message.text||!ctx.message.text.startsWith('/')) return;
-	
-	const commandText = ctx.message.text.match(/\/[a-z]*/)[0];
-	
-	if (!collection.has(commandText.substr(1))) return;
-	
-	let [commandName, ...args] = ctx.message.text.split(' ');
-	const command = collection.get(commandName.substr(1));
-	if (!command) return;
+  const commandText = ctx.message.text.match(/\/[a-z]*/)[0];
 
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments.\n\nUsage: /${command.name} ${command.usage}`
-		return ctx.reply(reply, { reply_to_message_id: ctx.message.message_id });
-	}
+  if (!collection.has(commandText.substr(1))) return;
 
-	try {
-		// Set chat action to 'typing' or 'sending a file'
-		ctx.telegram.sendChatAction(ctx.chat.id, command.chatAction);
-		if (command.name === 'help') return command.execute(ctx, args=args,  collection);
-		command.execute(ctx, args=args);
-	} catch(err) {
-		console.log(err);
-		ctx.reply(`Uhg, I ran into some errors, but don't worry it should be fixed soon`)
-	}
+  let [commandName, ...args] = ctx.message.text.split(' ');
+  const command = collection.get(commandName.substr(1));
+  if (!command) return;
+
+  if (command.args && !args.length) {
+    let reply = `You didn't provide any arguments.\n\nUsage: /${command.name} ${command.usage}`;
+    return ctx.reply(reply, { reply_to_message_id: ctx.message.message_id });
+  }
+
+  try {
+    // Set chat action to 'typing' or 'sending a file'
+    ctx.telegram.sendChatAction(ctx.chat.id, command.chatAction);
+    if (command.name === 'help')
+      return command.execute(ctx, (args = args), collection);
+    command.execute(ctx, (args = args));
+  } catch (err) {
+    console.log(err);
+    ctx.reply(
+      `Uhg, I ran into some errors, but don't worry it should be fixed soon`
+    );
+  }
 });
 
-app.get('/', (req, res) => res.send('bot online'))
+app.get('/', (req, res) => res.send('bot online'));
 
 // Function to start the bot
 function startBot() {
-	console.log("Bot is running...");
-	bot.launch();
+  console.log('Bot is running...');
+  bot.launch();
 }
 
 app.listen(3000, () => {
-	startBot();
-	cron.schedule('*/1 * * * *', () => {
-		bot.stop();
-		startBot();
-	})	
+  startBot();
+  cron.schedule('*/5 * * * *', () => {
+    bot.stop();
+    startBot();
+  });
 });
